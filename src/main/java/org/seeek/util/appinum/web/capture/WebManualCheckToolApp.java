32c014f0b1dc9be598d4f2fca42b0622fba49669
Yasuhiro Yamamoto <yasuhiro.y0807@gmail.com>
2017-08-24 18:36:51 +0900

    add appinum repository.

---
+++ src/main/java/org/seeek/util/appinum/web/capture/WebManualCheckToolApp.java
@@ -0,0 +1,12 @@
+package org.seeek.util.appinum.web.capture;
+
+public class WebManualCheckToolApp {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        System.out.println("Hello World.");
+
+    }
+
+}
